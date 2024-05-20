9511c4962f8dbc3cbd7025783b85aa36b2431d10
Zhi Lin <49969188+ZhiLinR@users.noreply.github.com>
2024-05-21 04:03:07 +0800

    OpenAI API tested
    tested and connected to Open AI's API for scenario generation

---
+++ microsvcs/gpt_generation/app/src/ext_api/api_gpt.ts
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+dotenv.config({ path: ['../.env.local', '../.env'] })
+import OpenAI from "openai";
+
+const openai = new OpenAI();
+
+async function main() {
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: "system", content: "Describe yourself." }],
+    model: "gpt-3.5-turbo",
+  });
+
+  console.log(completion.choices[0]);
+}
+
+main();
