b37f818cccc115741d7c5f7aca19e36a30e3304d
Zhi Lin <49969188+ZhiLinR@users.noreply.github.com>
2024-06-21 06:11:46 +0800

    Update handler.js
    minimal middleware??

--- microsvcs/forum/app/middleware/handler.js
+++ microsvcs/forum/app/middleware/handler.js
@@ -1,9 +1,4 @@
 exports.responseHandler = (result, req, res, next) => {
-    console.log("hit")
-    if (result.success) {
-        res.status(200).send(result);
-    }
-    else {
-        res.status(500).send({ error: 'Something failed!' });
-    }
+    res.status(result.status).send(result);
+
 }
